52734327fd26c0f6af3f8a83e58d3af5c54ba0ce
LeBenjos <benjamin.schinkel01@outlook.fr>
2025-10-27 17:45:54 +0100

    hotfix for prod

--- src/Experience/World/Gameboy.js
+++ src/Experience/World/Gameboy.js
@@ -1,9 +1,8 @@
-import * as THREE from 'three'
 import gsap from "gsap";
 import Experience from "../Experience";
 
-export default class Gameboy{
-    constructor(){
+export default class Gameboy {
+    constructor() {
         this.experience = new Experience()
         this.scene = this.experience.scene
         this.resources = this.experience.resources
@@ -12,13 +11,14 @@ export default class Gameboy{
 
         this.resource = this.resources.items.gameboyModel
         this.sound = {
-            play: (sound) =>{ 
+            play: (sound) => {
                 sound.currentTime = 0
-                sound.play() },
-            backgroundMusic: new Audio('/sounds/background.mp3'),
-            crossSelected: new Audio('/sounds/crossSelectedSound.mp3'),
-            easterEgg: new Audio('/sounds/easterEgg.mp3'),
-            boutonPress: new Audio('/sounds/boutonPress.mp3')
+                sound.play()
+            },
+            backgroundMusic: new Audio('/assets/gameboy/sounds/background.mp3'),
+            crossSelected: new Audio('/assets/gameboy/sounds/crossSelectedSound.mp3'),
+            easterEgg: new Audio('/assets/gameboy/sounds/easterEgg.mp3'),
+            boutonPress: new Audio('/assets/gameboy/sounds/boutonPress.mp3')
         }
         this.sound.backgroundMusic.loop = true
         this.sound.backgroundMusic.volume = 0.2
@@ -42,166 +42,166 @@ export default class Gameboy{
         this.setModel()
 
         window.addEventListener('keydown', (key) => {
-            if((key.code === 'KeyA' || key.code === 'KeyQ') && !this.key.a){
-                    this.easterEgg('KeyA')
-                    this.pressA()
+            if ((key.code === 'KeyA' || key.code === 'KeyQ') && !this.key.a) {
+                this.easterEgg('KeyA')
+                this.pressA()
             }
 
-            if(key.code === 'KeyB' && !this.key.b){
+            if (key.code === 'KeyB' && !this.key.b) {
                 this.easterEgg('KeyB')
                 this.pressB()
             }
 
-            if(key.code === 'ArrowUp' && !this.key.arrowUp){
+            if (key.code === 'ArrowUp' && !this.key.arrowUp) {
                 this.easterEgg('ArrowUp')
                 this.pressArrowUp()
             }
 
-            if(key.code === 'ArrowRight' && !this.key.arrowRight){
+            if (key.code === 'ArrowRight' && !this.key.arrowRight) {
                 this.easterEgg('ArrowRight')
                 this.pressArrowRight()
             }
 
-            if(key.code === 'ArrowDown' && !this.key.arrowDown){
+            if (key.code === 'ArrowDown' && !this.key.arrowDown) {
                 this.easterEgg('ArrowDown')
                 this.pressArrowDown()
             }
 
-            if(key.code === 'ArrowLeft' && !this.key.arrowLeft){
+            if (key.code === 'ArrowLeft' && !this.key.arrowLeft) {
                 this.easterEgg('ArrowLeft')
                 this.pressArrowLeft()
             }
-            
-            if(key.code === 'Escape' && !this.key.start){
+
+            if (key.code === 'Escape' && !this.key.start) {
                 this.pressStart()
             }
 
-            if(key.code === 'Tab' && !this.key.select){
+            if (key.code === 'Tab' && !this.key.select) {
                 this.pressSelect()
             }
         })
 
         window.addEventListener('keyup', (key) => {
-            if(key.code === 'KeyA' || key.code === 'KeyQ'){
+            if (key.code === 'KeyA' || key.code === 'KeyQ') {
                 this.unPressA()
             }
 
-            if(key.code === 'KeyB'){
+            if (key.code === 'KeyB') {
                 this.unPressB()
             }
 
-            if(key.code === 'ArrowUp'){
+            if (key.code === 'ArrowUp') {
                 this.unPressArrowUp()
             }
 
-            if(key.code === 'ArrowRight'){
+            if (key.code === 'ArrowRight') {
                 this.unPressArrowRight()
             }
 
-            if(key.code === 'ArrowDown'){
+            if (key.code === 'ArrowDown') {
                 this.unPressArrowDown()
             }
 
-            if(key.code === 'ArrowLeft'){
+            if (key.code === 'ArrowLeft') {
                 this.unPressArrowLeft()
             }
 
-            if(key.code === 'Escape'){
+            if (key.code === 'Escape') {
                 this.unPressStart()
             }
 
-            if(key.code === 'Tab'){
+            if (key.code === 'Tab') {
                 this.unPressSelect()
             }
         })
     }
 
-    setModel(){
+    setModel() {
         this.model = this.resource.scene
         this.scene.add(this.model)
 
         this.model.traverse((child) => {
-            if(child.isMesh){
+            if (child.isMesh) {
                 child.castShadow = true
             }
         })
     }
 
     // Key A
-    pressA(){
+    pressA() {
         this.key.a = true
         this.sound.play(this.sound.boutonPress)
         this.model.traverse((child) => {
-            if(child.name === "butonA"){
+            if (child.name === "butonA") {
                 gsap.to(child.position, { duration: 0.25, y: 3.1 })
             }
         })
     }
 
-    unPressA(){
+    unPressA() {
         this.key.a = false
         this.model.traverse((child) => {
-            if(child.name === "butonA"){
+            if (child.name === "butonA") {
                 gsap.to(child.position, { duration: 0.25, y: 3.2 })
             }
         })
     }
 
     // Key B
-    pressB(){
+    pressB() {
         this.key.b = true
         this.model.traverse((child) => {
-            if(child.name === "butonB"){
+            if (child.name === "butonB") {
                 gsap.to(child.position, { duration: 0.25, y: 3.1 })
             }
         })
     }
 
-    unPressB(){
+    unPressB() {
         this.key.b = false
         this.sound.play(this.sound.boutonPress)
         this.model.traverse((child) => {
-            if(child.name === "butonB"){
+            if (child.name === "butonB") {
                 gsap.to(child.position, { duration: 0.25, y: 3.2 })
             }
         })
     }
 
     // Key ArrowUp
-    pressArrowUp(){
+    pressArrowUp() {
         this.key.arrowUp = true
         this.model.traverse((child) => {
-            if(child.name === "cross"){
+            if (child.name === "cross") {
                 gsap.to(child.rotation, { duration: 0.25, x: -Math.PI * 0.025 })
             }
         })
     }
 
-    unPressArrowUp(){
+    unPressArrowUp() {
         this.key.arrowUp = false
         this.model.traverse((child) => {
-            if(child.name === "cross"){
-                gsap.to(child.rotation, { duration: 0.25, x: 0})
+            if (child.name === "cross") {
+                gsap.to(child.rotation, { duration: 0.25, x: 0 })
             }
         })
     }
 
     // Key ArrowRight
-    pressArrowRight(){
+    pressArrowRight() {
         this.key.arrowRight = true
         this.sound.play(this.sound.crossSelected)
         this.model.traverse((child) => {
-            if(child.name === "cross"){
+            if (child.name === "cross") {
                 gsap.to(child.rotation, { duration: 0.25, z: -Math.PI * 0.025 })
-                for(let i = 0; i < this.world.objet.length; i++){
-                    if(this.world.threeJsJourney.model.visible == true){
+                for (let i = 0; i < this.world.objet.length; i++) {
+                    if (this.world.threeJsJourney.model.visible == true) {
                         this.world.threeJsJourney.model.visible = false
                         this.world.objet[0].model.visible = true
                         return
-                    } else if(this.world.objet[i].model.visible == true){
-                        if(i+1 < this.world.objet.length){
+                    } else if (this.world.objet[i].model.visible == true) {
+                        if (i + 1 < this.world.objet.length) {
                             this.world.objet[i].model.visible = false
-                            this.world.objet[i+1].model.visible = true
+                            this.world.objet[i + 1].model.visible = true
                             return
                         } else {
                             this.world.objet[i].model.visible = false
@@ -214,50 +214,50 @@ export default class Gameboy{
         })
     }
 
-    unPressArrowRight(){
+    unPressArrowRight() {
         this.key.arrowRight = false
         this.model.traverse((child) => {
-            if(child.name === "cross"){
-                gsap.to(child.rotation, { duration: 0.25, z: 0})
+            if (child.name === "cross") {
+                gsap.to(child.rotation, { duration: 0.25, z: 0 })
             }
         })
     }
 
     // Key ArrowDown
-    pressArrowDown(){
+    pressArrowDown() {
         this.key.arrowDown = true
         this.model.traverse((child) => {
-            if(child.name === "cross"){
+            if (child.name === "cross") {
                 gsap.to(child.rotation, { duration: 0.25, x: Math.PI * 0.025 })
             }
         })
     }
 
-    unPressArrowDown(){
+    unPressArrowDown() {
         this.key.arrowDown = false
         this.model.traverse((child) => {
-            if(child.name === "cross"){
-                gsap.to(child.rotation, { duration: 0.25, x: 0})
+            if (child.name === "cross") {
+                gsap.to(child.rotation, { duration: 0.25, x: 0 })
             }
         })
     }
 
     // Key ArrowLeft
-    pressArrowLeft(){
+    pressArrowLeft() {
         this.key.arrowLeft = true
         this.sound.play(this.sound.crossSelected)
         this.model.traverse((child) => {
-            if(child.name === "cross"){
+            if (child.name === "cross") {
                 gsap.to(child.rotation, { duration: 0.25, z: Math.PI * 0.025 })
-                for(let i = 0; i < this.world.objet.length; i++){
-                    if(this.world.threeJsJourney.model.visible == true){
+                for (let i = 0; i < this.world.objet.length; i++) {
+                    if (this.world.threeJsJourney.model.visible == true) {
                         this.world.threeJsJourney.model.visible = false
                         this.world.objet[this.world.objet.length - 1].model.visible = true
                         return
-                    } else if(this.world.objet[i].model.visible == true){
-                        if(i-1 >= 0){
+                    } else if (this.world.objet[i].model.visible == true) {
+                        if (i - 1 >= 0) {
                             this.world.objet[i].model.visible = false
-                            this.world.objet[i-1].model.visible = true
+                            this.world.objet[i - 1].model.visible = true
                             return
                         } else {
                             this.world.objet[i].model.visible = false
@@ -270,46 +270,46 @@ export default class Gameboy{
         })
     }
 
-    unPressArrowLeft(){
+    unPressArrowLeft() {
         this.key.arrowLeft = false
         this.model.traverse((child) => {
-            if(child.name === "cross"){
-                gsap.to(child.rotation, { duration: 0.25, z: 0})
+            if (child.name === "cross") {
+                gsap.to(child.rotation, { duration: 0.25, z: 0 })
             }
         })
     }
-    
+
     // Key Start
-    
-    pressStart(){
+
+    pressStart() {
         this.key.start = true
         this.sound.play(this.sound.boutonPress)
         this.model.traverse((child) => {
-            if(child.name === "butonStart"){
-                gsap.to(child.position, { duration: 0.25, y: 3.1})
+            if (child.name === "butonStart") {
+                gsap.to(child.position, { duration: 0.25, y: 3.1 })
                 window.open("https://github.com/LeBenjos/ThreeJs_Journey_Challenge_001", "_blank");
             }
         })
     }
 
-    unPressStart(){
+    unPressStart() {
         this.key.start = false
         this.model.traverse((child) => {
-            if(child.name === "butonStart"){
-                gsap.to(child.position, { duration: 0.25, y: 3.2})
+            if (child.name === "butonStart") {
+                gsap.to(child.position, { duration: 0.25, y: 3.2 })
             }
         })
     }
-    
+
     // Key Select
-    
-    pressSelect(){
+
+    pressSelect() {
         this.key.select = true
         this.sound.play(this.sound.boutonPress)
         this.model.traverse((child) => {
-            if(child.name === "butonSelect"){
+            if (child.name === "butonSelect") {
                 gsap.to(child.position, { duration: 0.25, y: 3.1 })
-                if(!this.sound.backgroundMusic.paused){
+                if (!this.sound.backgroundMusic.paused) {
                     this.sound.backgroundMusic.pause()
                 } else {
                     this.sound.backgroundMusic.play()
@@ -318,31 +318,31 @@ export default class Gameboy{
         })
     }
 
-    unPressSelect(){
+    unPressSelect() {
         this.key.select = false
         this.model.traverse((child) => {
-            if(child.name === "butonSelect"){
-                gsap.to(child.position, { duration: 0.25, y: 3.2})
+            if (child.name === "butonSelect") {
+                gsap.to(child.position, { duration: 0.25, y: 3.2 })
             }
         })
     }
 
     // Easter Egg
 
-    easterEgg(key){
-        if(this.key.konami.length < 10){
+    easterEgg(key) {
+        if (this.key.konami.length < 10) {
             this.key.konami.push(key)
         } else {
             this.key.konami.shift()
             this.key.konami.push(key)
         }
 
-        if(JSON.stringify(this.key.konami) == JSON.stringify(this.key.easterEgg)){
+        if (JSON.stringify(this.key.konami) == JSON.stringify(this.key.easterEgg)) {
             this.sound.play(this.sound.easterEgg)
-            for(let i = 0; i < this.world.objet.length; i++){
-                if(this.world.objet[i].model.visible == true){
-                        this.world.objet[i].model.visible = false
-                        this.world.threeJsJourney.model.visible = true
+            for (let i = 0; i < this.world.objet.length; i++) {
+                if (this.world.objet[i].model.visible == true) {
+                    this.world.objet[i].model.visible = false
+                    this.world.threeJsJourney.model.visible = true
                 }
             }
             console.log("Thanks to ThreeJs Journey!")
--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -3,62 +3,62 @@ export default [
         name: 'environmentMapTexutre',
         type: 'cubeTexture',
         path: [
-            '/textures/env/px.png',
-            '/textures/env/nx.png',
-            '/textures/env/py.png',
-            '/textures/env/ny.png',
-            '/textures/env/pz.png',
-            '/textures/env/nz.png'
+            '/assets/gameboy/textures/env/px.png',
+            '/assets/gameboy/textures/env/nx.png',
+            '/assets/gameboy/textures/env/py.png',
+            '/assets/gameboy/textures/env/ny.png',
+            '/assets/gameboy/textures/env/pz.png',
+            '/assets/gameboy/textures/env/nz.png'
         ]
     },
     {
         name: 'gameboyModel',
         type: 'gltfModel',
-        path: '/models/gameboy/gameboy.glb'
+        path: '/assets/gameboy/models/gameboy/gameboy.glb'
     },
     {
         name: 'cartridgeModel',
         type: 'gltfModel',
-        path: '/models/gameboy/cartridge.glb'
+        path: '/assets/gameboy/models/gameboy/cartridge.glb'
     },
     {
         name: 'gameboyTextModel',
         type: 'gltfModel',
-        path: '/models/gameboy/gameboyText.glb'
+        path: '/assets/gameboy/models/gameboy/gameboyText.glb'
     },
     {
         name: 'logoModel',
         type: 'gltfModel',
-        path: '/models/gameboy/logo.glb'
+        path: '/assets/gameboy/models/gameboy/logo.glb'
     },
     {
         name: 'marioBoxModel',
         type: 'gltfModel',
-        path: '/models/gameboy/marioBox.glb'
+        path: '/assets/gameboy/models/gameboy/marioBox.glb'
     },
     {
         name: 'pokeballModel',
         type: 'gltfModel',
-        path: '/models/gameboy/pokeball.glb'
+        path: '/assets/gameboy/models/gameboy/pokeball.glb'
     },
     {
         name: 'tetrisModel',
         type: 'gltfModel',
-        path: '/models/gameboy/tetris.glb'
+        path: '/assets/gameboy/models/gameboy/tetris.glb'
     },
     {
         name: 'triforceModel',
         type: 'gltfModel',
-        path: '/models/gameboy/triforce.glb'
+        path: '/assets/gameboy/models/gameboy/triforce.glb'
     },
     {
         name: 'yoshiEggModel',
         type: 'gltfModel',
-        path: '/models/gameboy/yoshiEgg.glb'
+        path: '/assets/gameboy/models/gameboy/yoshiEgg.glb'
     },
     {
         name: 'threeJsJourneyModel',
         type: 'gltfModel',
-        path: '/models/gameboy/threeJsJourney.glb'
+        path: '/assets/gameboy/models/gameboy/threeJsJourney.glb'
     }
 ]
